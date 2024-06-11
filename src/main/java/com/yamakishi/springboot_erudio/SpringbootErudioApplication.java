ce3e800ac5bf91f87ee10c65344445ce4f01ff4d
maicom-yamakishi <maicom.yamakishi@gmail.com>
2024-06-11 15:29:53 -0300

    first commit

---
+++ src/main/java/com/yamakishi/springboot_erudio/SpringbootErudioApplication.java
@@ -0,0 +1,13 @@
+package com.yamakishi.springboot_erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootErudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yamakishi/springboot_erudio/SpringbootErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yamakishi.springboot_erudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
